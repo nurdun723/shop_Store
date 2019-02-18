a0a084d18116834370451c81c4856b162af31308
nurdun723 <2574842004@qq.com>
2019-02-19 00:27:00 +0800

    删除了一个注释

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import router from './router'
-//导入bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 //导入MUI
 import './lib/css/mui.min.css'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import App from './App'
+
 Vue.use(MintUI)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
